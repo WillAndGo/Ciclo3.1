0e41c073c342c9fa981fc08b92b89f5457d6a447
WillAndGo <awgonzalez@uninorte.edu.co>
2022-09-18 20:04:51 -0300

    HTML

---
+++ HTML/JS/validaciones.js
@@ -0,0 +1,17 @@
+function validar_formulario(){
+
+    var Username=document.registrouser.Username;
+    var Correo=document.registrouser.Correo;
+    var passwo=document.registrouser.password;
+
+    alert("Estoy ingresando a la funsión de validar" + Username)
+
+}
+
+function mensajenombre(){
+    alert("Estas en el campo Nombre")
+}
+
+function mensajecorreo(){
+    alert("Estas en el campo Correo")
+}
